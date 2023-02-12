cc0efea043ca527717174489bb260b1493efb70e
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-02-12 13:06:33 -0800

    Added Visibility, Title Style -so Benjamin

--- app/src/main/java/com/example/trackit/ui/theme/screens/SignInScreen.kt
+++ app/src/main/java/com/example/trackit/ui/theme/screens/SignInScreen.kt
@@ -3,10 +3,10 @@ package com.example.trackit.ui.theme.screens
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -16,6 +16,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.trackit.R
@@ -39,7 +40,10 @@ fun SignInScreen(
 
 @Composable
 fun Title() {
-    Text(text = stringResource(R.string.sign_in_welcome_text))
+    Text(
+        text = stringResource(R.string.sign_in_welcome_text),
+        style = MaterialTheme.typography.headlineMedium
+    )
 }
 
 @Composable
@@ -64,9 +68,9 @@ fun EmailTextField() {
 
 @Composable
 fun PasswordTextField() {
-    val passwordState = remember {
-        mutableStateOf(TextFieldValue())
-    }
+    val passwordState = remember { mutableStateOf(TextFieldValue()) }
+    val showPassword = remember { mutableStateOf(false) }
+
     TextField(
         modifier = Modifier
             .fillMaxWidth(),
@@ -82,13 +86,41 @@ fun PasswordTextField() {
             unfocusedIndicatorColor = Color.Transparent
         ),
         shape = RoundedCornerShape(8.dp),
-        visualTransformation = PasswordVisualTransformation(),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        visualTransformation = if (showPassword.value) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            if (showPassword.value) {
+                IconButton(
+                    onClick = { showPassword.value = false }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Visibility,
+                        contentDescription = stringResource(R.string.hide_password)
+                    )
+                }
+            } else {
+                IconButton(
+                    onClick = {
+                        showPassword.value = true
+
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.VisibilityOff,
+                        contentDescription = stringResource(R.string.show_password)
+                    )
+                }
+            }
+        }
     )
 }
 
 @Composable
-fun SignInButton(){
+fun SignInButton() {
     Button(
         onClick = { /*TODO*/ },
         modifier = Modifier
