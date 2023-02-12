b825846518ed924f3bab9c003dc970f4f56528e7
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-02-12 11:29:31 -0800

    Added Colors and Shape -so Benjamin

--- app/src/main/java/com/example/trackit/ui/theme/screens/SignInScreen.kt
+++ app/src/main/java/com/example/trackit/ui/theme/screens/SignInScreen.kt
@@ -1,13 +1,16 @@
 package com.example.trackit.ui.theme.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,13 +45,20 @@ fun EmailTextField() {
         mutableStateOf(TextFieldValue())
     }
     TextField(
+        modifier = Modifier
+            .fillMaxWidth(),
         value = emailState.value,
         onValueChange = {
             emailState.value = it
         },
         label = {
             Text(stringResource(R.string.email_hint_label))
-        }
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        shape = RoundedCornerShape(8.dp)
     )
 }
 
@@ -58,13 +68,20 @@ fun PasswordTextField() {
         mutableStateOf(TextFieldValue())
     }
     TextField(
+        modifier = Modifier
+            .fillMaxWidth(),
         value = passwordState.value,
         onValueChange = {
             passwordState.value = it
         },
         label = {
             Text(stringResource(R.string.password_hint_label))
-        }
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        shape = RoundedCornerShape(8.dp)
     )
 }
 
