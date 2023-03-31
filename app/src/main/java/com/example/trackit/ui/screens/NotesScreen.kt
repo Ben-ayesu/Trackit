36bc4cfd1d05ab999c78f5605cbdd7f5ee642784
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-03-30 19:19:15 -0700

    Added NotesScreen.kt and Preview
    Signed-off-by: benjaminayesu-attah <ben.ayesu.attah@gmail.com>

---
+++ app/src/main/java/com/example/trackit/ui/screens/NotesScreen.kt
@@ -0,0 +1,51 @@
+package com.example.trackit.ui.screens
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NotesScreen() {
+    val context = LocalContext.current
+    val elements = MutableList(100) { it }
+    LazyColumn(
+        modifier = Modifier.background(MaterialTheme.colorScheme.background)
+    ) {
+        items(elements) {
+            Row(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(4.dp))
+                    .padding(4.dp)
+                    .clickable {
+                        Toast
+                            .makeText(context, "Item $elements", Toast.LENGTH_SHORT)
+                            .show()
+                    },
+            ) {
+                Text(text = "Element ${elements[it]}")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NoteScreenPreview() {
+    NotesScreen()
+}
