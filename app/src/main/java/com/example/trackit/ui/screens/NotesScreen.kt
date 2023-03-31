300f9fce50fcda1fc947591fc5d90b789321de4e
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-03-30 21:12:44 -0700

    Added Action Title
    Signed-off-by: benjaminayesu-attah <ben.ayesu.attah@gmail.com>

--- app/src/main/java/com/example/trackit/ui/screens/NotesScreen.kt
+++ app/src/main/java/com/example/trackit/ui/screens/NotesScreen.kt
@@ -3,6 +3,7 @@ package com.example.trackit.ui.screens
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,28 +18,39 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun NotesScreen() {
+
     val context = LocalContext.current
     val elements = MutableList(100) { it }
-    LazyColumn(
-        modifier = Modifier.background(MaterialTheme.colorScheme.background)
-    ) {
-        items(elements) {
-            Row(
-                modifier = Modifier
-                    .padding(4.dp)
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(4.dp))
-                    .padding(4.dp)
-                    .clickable {
-                        Toast
-                            .makeText(context, "Item $elements", Toast.LENGTH_SHORT)
-                            .show()
-                    },
-            ) {
-                Text(text = "Element ${elements[it]}")
+
+    Column() {
+        Text(
+            modifier = Modifier
+                .padding(8.dp),
+            text = "Create a note",
+            fontSize = 32.sp
+        )
+        LazyColumn(
+            modifier = Modifier.background(MaterialTheme.colorScheme.background)
+        ) {
+            items(elements) {
+                Row(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(4.dp))
+                        .padding(4.dp)
+                        .clickable {
+                            Toast
+                                .makeText(context, "Item $elements", Toast.LENGTH_SHORT)
+                                .show()
+                        },
+                ) {
+                    Text(text = "Element ${elements[it]}")
+                }
             }
         }
     }
