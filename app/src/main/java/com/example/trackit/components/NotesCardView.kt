6237dd969073bb3f01c5fdc9a36f3fb1c329d54f
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-04-01 13:26:39 -0700

    Added Action Title
    Signed-off-by: benjaminayesu-attah <ben.ayesu.attah@gmail.com>

--- app/src/main/java/com/example/trackit/components/NotesCardView.kt
+++ app/src/main/java/com/example/trackit/components/NotesCardView.kt
@@ -1,38 +1,32 @@
 package com.example.trackit.components
 
-import androidx.compose.foundation.Image
+import android.icu.text.SimpleDateFormat
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.trackit.R
+import java.util.Date
 
 @Preview
 @Composable
 fun NotesCard() {
+
+    val sdf = SimpleDateFormat("'Date: 'dd-MM-yyyy '\nTime:'HH:mm:ss z")
+    val currentDateAndTime = sdf.format(Date())
+
     Surface(
         shape = RoundedCornerShape(16.dp),
         color = MaterialTheme.colorScheme.primaryContainer,
@@ -51,93 +45,11 @@ fun NotesCard() {
                     .weight(2f),
                 verticalArrangement = Arrangement.Center
             ) {
-                Surface(
-                    shape = RoundedCornerShape(24.dp),
-                    modifier = Modifier.wrapContentSize(),
-                    color = Color(0xFFD1D5E1)
-                ) {
-                    Text(
-                        text = "New release",
-                        fontSize = 12.sp,
-                        style = MaterialTheme.typography.titleLarge,
-                        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-                    )
-                }
-
-                Spacer(modifier = Modifier.height(4.dp))
-
                 Text(
-                    text = "Cute Cat toy",
+                    text = currentDateAndTime,
                     fontSize = 24.sp,
-                    style = MaterialTheme.typography.titleLarge,
+                    style = MaterialTheme.typography.titleMedium,
                     fontWeight = FontWeight.SemiBold
-                )
-
-                Spacer(modifier = Modifier.height(2.dp))
-
-                Text(text = "Price : 300$")
-
-                Spacer(modifier = Modifier.height(2.dp))
-
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Text(
-                        text = "4.0",
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.SemiBold,
-                        style = MaterialTheme.typography.titleLarge
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_star_outline_24),
-                        tint = Color(0xFFF6B266),
-                        contentDescription = null
-                    )
-
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_star_outline_24),
-                        tint = Color(0xFFF6B266),
-                        contentDescription = null
-                    )
-
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_star_outline_24),
-                        tint = Color(0xFFF6B266),
-                        contentDescription = null
-                    )
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_star_outline_24),
-                        tint = Color(0xFFF6B266),
-                        contentDescription = null
-                    )
-                }
-
-                Spacer(modifier = Modifier.height(4.dp))
-
-                OutlinedButton(
-                    shape = RoundedCornerShape(8.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.Black,
-                        containerColor = Color.White
-                    ),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Text(
-                        text = "Read More",
-                        fontSize = 11.sp,
-                        fontWeight = FontWeight.SemiBold,
-                        style = MaterialTheme.typography.titleLarge
-                    )
-                }
-            }
-
-            Surface(
-                shape = RoundedCornerShape(16.dp),
-                modifier = Modifier.size(width = 100.dp, height = 140.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.panda),
-                    contentScale = ContentScale.Crop,
-                    contentDescription = null
                 )
             }
         }
